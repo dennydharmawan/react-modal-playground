d1bcde268e4d0f31ccfd468d937e4e1ef6f5fde6
denny dharmawan <denny.dharmawan@btpn.com>
2020-08-07 20:03:45 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
